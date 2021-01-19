944dc705f3c206bda9fd4b51ec74066fc76394be
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-01-19 09:54:35 +0530

    Configured i118next

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import userRouter from "./user/userRouter";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import middleware from "i18next-http-middleware";
+
+i18next
+  .use(Backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    lng: "en",
+    ns: ["translation"],
+    defaultNS: "translation",
+    backend: {
+      loadPath: "./locales/{{lng}}/{{ns}}/.json",
+      detection: {
+        lookupHeader: "accept-language",
+      },
+    },
+  });
 
 const app = express();
 
 // parse JSON
 app.use(express.json());
+
+// support i18
+app.use(middleware.handle(i18next));
 
 // routers
 app.use("/api/1.0/users", userRouter);
