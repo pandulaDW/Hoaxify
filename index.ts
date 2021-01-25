ca41917c11f798e5484d177420c77b6145d69a3e
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-01-26 00:12:27 +0530

    Removed unwanted files from repo

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import app from "./src/app";
 import sequelize from "./src/config/database";
 
+// noinspection JSIgnoredPromiseFromCall
 sequelize.sync({ force: true });
 
 app.listen(5000, () => {
