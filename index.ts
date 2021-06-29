6554c783c0fb1fed4361ce2c19a973003dd72247
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-06-29 20:19:12 +0530

    redoing

--- index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./src/app";
-import sequelize from "./src/config/database";
-
-// noinspection JSIgnoredPromiseFromCall
-sequelize.sync({ force: true });
-
-app.listen(5000, () => {
-  console.log("Starting the server...");
-});
